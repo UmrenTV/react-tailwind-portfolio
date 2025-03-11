e56a7d7355bf89ec7299712f1ff4dc8bc60b6ae2
Jane Zdravevski <jane@zdravevski.pro>
2025-03-11 16:39:27 +0100

    added basic README and modified some icons and data

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import heroImg from "../assets/hero.svg";
-import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
 
 const Hero = () => {
     return (
@@ -10,20 +11,23 @@ const Hero = () => {
                         I'm Jane
                     </h1>
                     <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide ">
-                        Front-End Developer
+                        Full-Stack Developer
                     </p>
-                    <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
-                        turning ideas into interactive reality
+                    <p className="mt-2 text-lg text-slate-700 tracking-wide">
+                        I create Solutions out of Problems
                     </p>
                     <div className="flex gap-x-4 mt-4">
-                        <a href="#" target="_blank">
+                        <a href="https://zdravevski.pro" target="_blank">
+                            <TbWorldWww className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+                        </a>
+                        <a href="https://github.com/umrentv" target="_blank">
                             <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
                         </a>
-                        <a href="#" target="_blank">
+                        <a
+                            href="https://linkedin.com/in/zdravevski"
+                            target="_blank"
+                        >
                             <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
-                        </a>
-                        <a href="#" target="_blank">
-                            <FaTwitterSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
                         </a>
                     </div>
                 </article>
